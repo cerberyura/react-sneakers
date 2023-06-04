d270a3cb84597869188f31360349091a8ad23f74
Yurii Cerber <yuriicerber@gmail.com>
2023-06-04 19:57:21 +0000

    norm

---
+++ src/components/Drawer/index.js
@@ -0,0 +1,61 @@
+import React from "react";
+import styles from './Drawer.module.scss'
+
+export const Drawer = () => {
+  return (
+    <div className={styles.overlay}>
+      <div className={styles.drawer}>
+      <div className="items">
+        <h2 className="mb-40 d-flex justify-between align-center ">
+          Кошик
+          <img className="removeBtn cu-p" width={32} height={32} src="/img/btn-remove.svg" alt="remove"/>
+        </h2>
+
+        <div className="cartItem d-flex align-center">
+          <img
+            className="cartItemImg"
+            src="/img/sneakers/1.jpg" alt="sneakers"/>
+          <div className="mr-20">
+            <p className="mb-5">Чоловічі Кросівки Nike Blazer Mid Suede</p>
+            <b>5000 ₴</b>
+          </div>
+          <img className="removeBtn" width={32} height={32} src="/img/btn-remove.svg" alt="remove"/>
+        </div>
+        <div className="cartItem d-flex align-center">
+          <img
+            className="cartItemImg"
+            src="/img/sneakers/2.jpg" alt="sneakers"/>
+          <div className="mr-20">
+            <p className="mb-5">Чоловічі Кросівки Nike Blazer Mid Suede</p>
+            <b>5000 ₴</b>
+          </div>
+          <img className="removeBtn" width={32} height={32} src="/img/btn-remove.svg" alt="remove"/>
+        </div>
+
+
+      </div>
+
+
+
+        <div className="items">
+          <div className="cartTotalBlock">
+            <ul>
+              <li className="d-flex">
+                <span>Сума</span>
+                <div></div>
+                <b>15500 ₴.</b>
+              </li>
+              <li className="d-flex">
+                <span>Налог 5%:</span>
+                <div></div>
+                <b>457 ₴</b>
+              </li>
+            </ul>
+            <button className="greenButton">Оформити замовлення <img src="/img/arrow.svg" alt="arrow"/></button>
+          </div>
+        </div>
+
+      </div>
+    </div>
+  )
+}
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,11 @@
+import {Drawer} from "./Drawer";
+
 export const Header = () => {
   return (
     <div className="wrapper clear">
+
+     <Drawer/>
+
       <header className="d-flex justify-between align-center p-40">
         <div className="d-flex align-center">
           <img className='logo' src="/img/logo.png" alt="logo"/>
@@ -23,10 +28,19 @@ export const Header = () => {
       </header>
 
       <div className="content p-40">
-        <h1 className="mb-40">Всі коросівки</h1>
+        <div className="d-flex mb-40 justify-between">
+          <h1>Всі кросівки</h1>
+          <div className="search-block d-flex">
+            <img src="/img/search.svg" alt="search"/>
+            <input type="text" placeholder="Пошук..."/>
+          </div>
+        </div>
 
         <div className="d-flex">
           <div className="card">
+            <div className="favorite">
+              <img src="/img/unliked.svg" alt="unliked"/>
+            </div>
             <img width={133} height={112} src="/img/sneakers/1.jpg" alt="Sneakers"/>
             <h5>Чоловічі Кросівки Nike Blazer Mid Suede</h5>
             <div className="d-flex justify-between align-center">
