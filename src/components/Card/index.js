4aa177dddaa60b96920ae8d420e383d3f47846b5
Yurii Cerber <yuriicerber@gmail.com>
2023-06-07 22:09:16 +0000

    reactSneake

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,21 +1,26 @@
 import React from "react";
 import './Card.module.scss';
-export const Card = () => {
+
+
+const onClickPlus= () => {
+  alert('plus')
+}
+export const Card = ({title, price, imageUrl}) => {
   return (
     <div className="card">
       <div className="favorite">
         <img src="/img/unliked.svg" alt="unliked"/>
       </div>
-      <img width={133} height={112} src="/img/sneakers/1.jpg" alt="Sneakers"/>
-      <h5>Чоловічі Кросівки Nike Blazer Mid Suede</h5>
+      <img width={133} height={112} src={imageUrl} alt="Sneakers"/>
+      <h5>{title}</h5>
       <div className="d-flex justify-between align-center">
         <div>
           <div className="d-flex flex-column">
             <span>Ціна: </span>
-            <b>5000 ₴</b>
+            <b>{price} ₴</b>
           </div>
         </div>
-        <button className="button">
+        <button className="button" onClick={onClickPlus}>
           <img width={11} height={11} src="/img/plus.svg" alt="plus"/>
         </button>
       </div>
